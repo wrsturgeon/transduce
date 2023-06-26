218af4c239cebef2a7b9d9ac32ae282227ffbf52
Will Sturgeon <willstrgn@gmail.com>
2023-06-26 11:50:31 -0700

    Successful `flake.nix`

--- flake.nix
+++ flake.nix
@@ -8,8 +8,9 @@
   outputs = { flake-utils, naersk, nixpkgs, self }: flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = (import nixpkgs) { inherit system; };
+      naersk' = pkgs.callPackage naersk {};
     in {
-      defaultPackage = naersk.buildPackage { src = ./.; };
+      defaultPackage = naersk'.buildPackage { src = ./.; };
       devShell = pkgs.mkShell { nativeBuildInputs = with pkgs; [ rustc cargo ]; };
     }
   );
