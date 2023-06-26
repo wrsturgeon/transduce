46c73ed14b33f0eba8636c675ab8a8dba49e7583
Will Sturgeon <willstrgn@gmail.com>
2023-06-26 11:49:04 -0700

    Replace with Naersk's suggested flake.nix

--- flake.nix
+++ flake.nix
@@ -1,56 +1,16 @@
-# Edited from https://hoverbear.org/blog/a-flake-for-your-crate/
 {
   description = "Zero-copy isomorphic parsing: your code should look like what it parses.";
   inputs = {
-    naersk = {
-      inputs.nixpkgs.follows = "nixpkgs";
-      url = "github:nmattia/naersk";
-    };
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nmattia/naersk";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
-  outputs = { naersk, nixpkgs, self }:
-  let
-    cargo-toml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
-    for-all-systems = f: nixpkgs.lib.genAttrs supported-systems (system: f system);
-    supported-systems = [ "x86_64-linux" "aarch-linux" "x86_64-darwin" ];
-  in {
-    checks = for-all-systems (system:
-      let
-        pkgs = import nixpkgs { overlays = [ self.overlay ]; inherit system; };
-      in {
-      "${cargo-toml.package.name}" = pkgs."${cargo-toml.package.name}";
-        format = let
-          build-inputs = { buildInputs = with pkgs; [ rustfmt cargo ]; };
-          commands = ''
-            ${pkgs.rustfmt}/bin/cargo-fmt fmt --manifest-path ${./.}/Cargo.toml -- --check
-            ${pkgs.nixpkgs-fmt} --check ${./.}
-            touch $out
-          '';
-        in
-          pkgs.runCommand "check-format" build-inputs commands;
-      }
-    );
-    defaultPackage = for-all-systems (system: (import nixpkgs { overlays = [ self.overlay ]; inherit system; })."${cargo-toml.package.name}");
-    devShell = for-all-systems (system:
-      let
-        pkgs = import nixpkgs {
-          overlays = [ self.overlay ];
-          inherit system;
-        };
-      in 
-        pkgs.mkShell {
-          inputsFrom = [ pkgs."${cargo-toml.package.name}" ];
-          builtInputs = with pkgs; [ rustfmt nixpkgs-fmt ];
-          LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
-        }
-    );
-    overlay = final: prev: { "${cargo-toml.package.name}" = final.callPackage ./. { inherit naersk; }; };
-    packages = for-all-systems (system:
-      let
-        pkgs = import nixpkgs { overlays = [ self.overlay ]; inherit system; };
-      in {
-        "${cargo-toml.package.name}" = pkgs."${cargo-toml.package.name}";
-      }
-    );
-  };
+  outputs = { flake-utils, naersk, nixpkgs, self }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = (import nixpkgs) { inherit system; };
+    in {
+      defaultPackage = naersk.buildPackage { src = ./.; };
+      devShell = pkgs.mkShell { nativeBuildInputs = with pkgs; [ rustc cargo ]; };
+    }
+  );
 }
